f6d7db26d05928706f9d18b9fcd81aade1f5f375
axel <684824+axelfontaine@users.noreply.github.com>
2024-01-10 15:55:51 +0100

    Dropped nosuid on bind mounts

--- x64.cmd
+++ x64.cmd
@@ -1,2 +1,2 @@
 docker buildx build --platform=linux/amd64 --progress=plain --output type=local,dest=build/x64 .
-docker run --rm --privileged -v x:\workspaces\bubblewrap\build\x64\bubblewrap.tar.gz:/bubblewrap.tar.gz alpine sh -c "apk add -q libcap setpriv && tar xzf /bubblewrap.tar.gz && /usr/bin/bwrap --cap-drop ALL --cap-add CAP_SYSLOG --bind / / --uid 1000 --gid 1234 setpriv -d"
+docker run --rm --privileged -v x:\workspaces\bubblewrap\build\x64\bubblewrap.tar.gz:/bubblewrap.tar.gz alpine sh -c "apk add -q libcap setpriv && tar xzf /bubblewrap.tar.gz && /usr/bin/bwrap --cap-drop ALL --cap-add CAP_SYSLOG --bind / / --uid 1000 --gid 1234 sh -c 'setpriv -d && mount'"
